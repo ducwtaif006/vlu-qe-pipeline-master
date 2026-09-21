4b48d139920fdab0d8f6ada063075ed7567c2796
Bùi Đức Tài <tai.2474802010341@vanlanguni.vn>
2026-09-21 13:48:28 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ function login(username, password) {
     }
 
     // Đăng nhập đúng
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "1234") {
         return true;
     }
 
